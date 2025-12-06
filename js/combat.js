69d4b19d7bebbcf49447aea87b74ead50feaa2df
madenciicom <ayazyd20@gmail.com>
2025-12-06 17:10:57 +0300

    Implement combat end dialogue logic

--- js/combat.js
+++ js/combat.js
@@ -1,1 +1,18 @@
-
+// Savaşın bitişi
+function combatEnd(victory, nextChapter) {
+    if (victory) {
+        UI.setDialogue(
+            "Zafer! Devam et.",
+            [
+                { label: "Devam", action: () => showChapter(nextChapter) }
+            ]
+        );
+    } else {
+        UI.setDialogue(
+            "Kaybettin... Tekrar dene.",
+            [
+                { label: "Yeniden Başla", action: () => startCombat(lastEnemy, nextChapter) }
+            ]
+        );
+    }
+}
