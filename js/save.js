3357e181535b02f4901ec8132f5c9994520c6708
madenciicom <ayazyd20@gmail.com>
2025-12-06 17:31:53 +0300

    Implement basic game save and load functions

--- js/save.js
+++ js/save.js
@@ -1,1 +1,15 @@
+// Basit kayıt sistemi
 
+function saveGame(state) {
+    localStorage.setItem("kai_game_save", JSON.stringify(state));
+}
+
+function loadGame() {
+    const data = localStorage.getItem("kai_game_save");
+    if (!data) return null;
+    return JSON.parse(data);
+}
+
+function clearSave() {
+    localStorage.removeItem("kai_game_save");
+}
