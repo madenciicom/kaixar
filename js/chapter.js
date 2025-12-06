e4d57bec3962644badf58eac566e73e6062a5be2
madenciicom <ayazyd20@gmail.com>
2025-12-06 17:12:34 +0300

    Add chapter 2 and weapon interaction functions

--- js/chapter.js
+++ js/chapter.js
@@ -27,3 +27,45 @@ function chapter1_escape() {
         ]
     );
 }
+function chapter2() {
+    UI.renderScene({}, {});
+
+    UI.setDialogue(
+        "Kai: Bu terk edilmiş kamp… Burada birileri yaşamış olmalı.",
+        [
+            { label: "Etrafı incele", action: () => chapter2_part2() }
+        ]
+    );
+}
+
+function chapter2_part2() {
+    UI.setDialogue(
+        "Kai, yerde paslanmış bir silah parçası bulur. Görünüşe göre geliştirilebilir.",
+        [
+            { label: "Silahı al", action: () => chapter2_getWeapon() },
+            { label: "Boşver", action: () => chapter2_skipWeapon() }
+        ]
+    );
+}
+
+function chapter2_getWeapon() {
+    UI.log("Silah yükseltildi! Hasar +5");
+    player.weapon = "Gelişmiş Çubuk";
+    player.damageBoost = 5;
+
+    UI.setDialogue(
+        "Silahı aldın! Aniden bir ayak sesi…",
+        [
+            { label: "Hazırlan", action: () => startCombat({ name: "Kamp Haydudu", hp: 50 }, 3) }
+        ]
+    );
+}
+
+function chapter2_skipWeapon() {
+    UI.setDialogue(
+        "Silahı almadın… Ama birisi yaklaşıyor!",
+        [
+            { label: "Dön ve savaş", action: () => startCombat({ name: "Kamp Haydudu", hp: 50 }, 3) }
+        ]
+    );
+}
