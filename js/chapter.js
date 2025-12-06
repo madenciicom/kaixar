b2eaa55cb0c359e9af982d0df20a9ca8e4e68c9c
madenciicom <ayazyd20@gmail.com>
2025-12-06 17:10:42 +0300

    Add chapter1 and related functions for dialogue

--- js/chapter.js
+++ js/chapter.js
@@ -1,1 +1,29 @@
+function chapter1() {
+    UI.renderScene({}, {}); // Boş sahne
 
+    UI.setDialogue(
+        "Kai: Nerede olduğumu hatırlamıyorum... Ama önümde bir yol var.",
+        [
+            { label: "İlerle", action: () => chapter1_part2() }
+        ]
+    );
+}
+
+function chapter1_part2() {
+    UI.setDialogue(
+        "Ormanın derinliklerinde bir gölge hareket ediyor...",
+        [
+            { label: "Yaklaş", action: () => startCombat({ name: "Orman Gölgesi", hp: 40 }, 2) },
+            { label: "Kaç", action: () => chapter1_escape() }
+        ]
+    );
+}
+
+function chapter1_escape() {
+    UI.setDialogue(
+        "Kaçmaya çalıştın ama gölge seni buldu!",
+        [
+            { label: "Savaş", action: () => startCombat({ name: "Orman Gölgesi", hp: 40 }, 2) }
+        ]
+    );
+}
